c738201a164954b09f203b0864162bd35030f3e8
Yeonjae-Cho <72658768+Yeonjae-Cho@users.noreply.github.com>
2021-05-16 23:58:11 +0900

    print2

---
+++ Two/CU2.py
@@ -0,0 +1,25 @@
+#6009
+a = input()
+print(a)
+
+#6010
+n = input()
+n = int(n)
+print(n)
+
+#6011
+f = input()
+f = float(f)
+print(f)
+
+#6012
+a = input() 
+b = input()
+print(a)
+print(b)
+
+#6013
+a=input()
+b=input()
+print(b)
+print(a)
